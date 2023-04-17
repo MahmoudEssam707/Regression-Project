fd66abf7ab9863843e110acaf5f9554873d97fa5
Mahmoud Essam <mahmoudessam0123@gmail.com>
2023-04-17 22:38:44 +0200

    Last project

--- Project Regression.R
+++ Project Regression.R
@@ -8,13 +8,13 @@
 # Confidence interval for estimator at given significant level(Checked)(gaber and safy).
 # Calculation of C.l. for mean response and new observation(Gowely and Ashraf).
 # Scatter plot contain fitted line(Checked)(Mahmoud).
+path <- readline("Please put your path :")
+Data <- read.csv(path)
+SL <- as.numeric(readline("Enter significance level : "))
+Xn = as.numeric(readline("Enter the X for mean response: "))
+Xnew = as.numeric(readline("Enter the X for new observation: "))
+SLR <- function(Data,SL,Xn,Xnew){
 #----------------------Mahmoud Essam and Zyad Ashraf--------------------------#
-Data <- data.frame(
-  x = c(1,2,4,6,8),
-  y = c(4,3,1,2,0)
-)
-n=length(y)
-t <- qt(alpha/2, n - 2,lower.tail = F)
 # Getting needed data
 x <- Data$x
 y <- Data$y
@@ -42,14 +42,13 @@ SST <-Syy
 SSE <-SST-SSR
 #calculating coeffecient(correlation, determination)
 Rsquare<-as.numeric(SSR/SST)
-print(paste('dependent variable explained by an independent variable in regression model ',Rsquare*100,'%'))
+print(paste('dependent variable explained by an independent variable in regression model',Rsquare*100,'%'))
 if(Beta_1<0){
   r<-0-sqrt(Rsquare)
-  print(paste("the model has negative correlation about ",r))
-  
+  print(paste("the model has negative correlation about",r))
 }else{
   r<-sqrt(Rsquare)
-  print(paste("the model has positive correlation about ",r))
+  print(paste("the model has positive correlation about",r))
 }
 #----------------------mohamed hamdy and bishoy-------------------------------#
 #-----------------------------------------------------------------------------#
@@ -68,10 +67,8 @@ ANOVA=matrix(c(SSR,SSE,SST,DFR,DFE,DFT,MSR,MSE,"",F0,"",""),ncol=4)
 row.names(ANOVA)=c("Treatment","Error","Total")
 colnames(ANOVA)=c("Sum square","Degree of freedom","Mean sum square","F table")
 ANOVA<- as.table(ANOVA)
-ANOVA
 # calculate f_test 
-alpha <- as.numeric(readline("Enter significance level : "))
-Fc<- qf(alpha, DFR, DFE)
+Fc<- qf(SL, DFR, DFE)
 if (F0 > Fc) {
   print("Reject H0, There's relation between X and Y")
 } else {
@@ -82,67 +79,58 @@ if (F0 > Fc) {
 #----------------------Mohamed Hassan and Safy Fathy--------------------------#
 #Confidence Interval For B0 
 Confidence_Interval_of_B1 = function(C){
-  c = 1-((1-C)/2)
-  t = qt(c,df=(n-2))
+  t = qt(C/2,df=(n-2))
   margin =  t * sqrt(MSE/Sxx)
   lower_bound = Beta_1-margin
   upper_bound = Beta_1+margin
   CI = c(lower_bound,upper_bound)
   return(CI)
-  
 }
-B1 = Confidence_Interval_of_B1(0.95)
-
-
 Confidence_Interval_of_B0 = function(C){
-  c = 1-((1-C)/2)
-  t = qt(c,df=(n-2))
+  t = qt(C/2,df=(n-2))
   margin =  t * sqrt(MSE*((1/n)+(xbar^2/Sxx)))
   lower_bound = Beta_0-margin
   upper_bound = Beta_0+margin
   CI = c(lower_bound,upper_bound) 
   return(CI)
-  
 }
-
-B0 = Confidence_Interval_of_B0(0.95)
-print(paste0("B0 is between interval " , data.frame(B0) ))
-print(paste0("B1 is between interval " , data.frame(B1)))
+B0 = Confidence_Interval_of_B0(SL)
+lower_bound_b0 = B0[2]
+upper_bound_b0 = B0[1]
+B1 = Confidence_Interval_of_B1(SL)
+lower_bound_b1 = B1[2]
+upper_bound_b1 = B1[1]
 #----------------------Mohamed Hassan and Safy Fathy--------------------------#
 #-----------------------------------------------------------------------------#
-
+#--------------------Abdelrhman Ashraf and Abdullah Hussein--------------------#
+RatX=Beta_0+Beta_1*Xn
+t=qt(SL/2, length(x)-2,lower.tail = F)
+CL = as.numeric((1-SL)*100)
+L=RatX-t*sqrt(MSE*(1/length(x)+(Xn-xbar)^2/Sxx))
+U=RatX+t*sqrt(MSE*(1/length(x)+(Xn-xbar)^2/Sxx))
+RatXnew=Beta_0+Beta_1*Xnew
+t=qt(SL/2, length(x)-2,lower.tail = F)
+CL = as.numeric((1-SL)*100)
+Lnew= RatXnew - t * sqrt(MSE*(1+1/length(x)+(Xnew-xbar)^2/Sxx))
+Unew= RatXnew + t * sqrt(MSE*(1+1/length(x)+(Xnew-xbar)^2/Sxx))
 #--------------------Abdelrhman Ashraf and Abdullah Hussein--------------------#
 #------------------------------------------------------------------------------#
-
-Xn = as.numeric(readline("Enter the X for mean response: "))
-RatX=B0+B1*Xn
-RatX
-
-alpha = 0.05
-t=qt(alpha/2, length(x)-2,lower.tail = F)
-t
-
-CL = as.numeric((1-alpha)*100)
-L= RatX - t * sqrt(MSE*(1/length(x)+(Xn-Xbar)^2/Sxx))
-U=RatX+ t * sqrt(MSE*(1/length(x)+(Xn-Xbar)^2/Sxx))
-
-print(paste("Confidence Intervat for mean response at confidence level", CL,"%"))
-print(paste("[",L, ",",U,"]"))
-
-Xnew = as.numeric(readline("Enter the X for new observation: "))
-RatXnew=B0+B1*Xnew
-RatXnew
-
-alpha = 0.05
-t=qt(alpha/2, length(x)-2,lower.tail = F)
-t
-
-CL = as.numeric((1-alpha)*100)
-Lnew= RatXnew - t * sqrt(MSE*(1+1/length(x)+(Xnew-Xbar)^2/Sxx))
-Unew= RatXnew + t * sqrt(MSE*(1+1/length(x)+(Xnew-Xbar)^2/Sxx))
-
-print(paste("Confidence Intervat for new observation at confidence level", CL,"%"))
-print(paste("[",Lnew, ",",Unew,"]"))
-
-#--------------------Abdelrhman Ashraf and Abdullah Hussein--------------------#
-#------------------------------------------------------------------------------#
+#Print Functions
+message(cat("Value of Sxx is",Sxx
+    ,"\nValue of Syy is",Syy
+    ,"\nValue of Sxy is",Sxy
+    ,"\nValue of Beta One(slope) is",Beta_1
+    ,"\nValue of Beta node(intercept) is",Beta_0
+    ,"\nValue of SST =",Syy
+    ,"\nValue of SSR =",SSR
+    ,"\nValue of SSE =",SSE
+    ,'\nTarget explained by an Features in regression model',Rsquare*100,'%'
+    ,"\n",lower_bound_b0,"<B0<",upper_bound_b0
+    ,"\n",lower_bound_b1, "<B1<",upper_bound_b1
+    ,"\nConfidence Interval for mean response at confidence level",CL,"%"
+    ,"\n[",L, ",",U,"]"
+    ,"\nConfidence Interval for new observation at confidence level",CL,"%"
+    ,"\n[",Lnew, ",",Unew,"]"))
+ANOVA
+}
+SLR(Data,SL,Xn,Xnew)
